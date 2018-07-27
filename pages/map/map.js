24024cf937e448997e91152eb1a153558eb6e25e
liuguoyuan <2805500299@qq.com>
2018-07-27 13:49:55 +0800

    添加城市选择器

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,5 +1,7 @@
 var qqmap = require('../../libs/qqmap-wx-jssdk.min.js');
-var qqmapsdk;
+var qqmapsdk = new qqmap({
+  key: "PKRBZ-M7MCW-7EIRF-OOUMT-EW6TQ-S4BFY"
+});
 Page({
   data: {
     latitude: 23.099994,
@@ -43,9 +45,6 @@ Page({
   //把当前位置的经纬度传给高德地图，调用高德API获取当前地理位置，天气情况等信息
   loadCity: function (latitude, longitude) {
     var that = this;
-    qqmapsdk = new qqmap({
-      key: "PKRBZ-M7MCW-7EIRF-OOUMT-EW6TQ-S4BFY"
-    }) 
     qqmapsdk.reverseGeocoder({
       location: {
         latitude: latitude,
@@ -58,6 +57,7 @@ Page({
         var res = res.result;
         that.setData({
           address: res.address,
+          city: res.address_component.city,
           markers: [{
             id: 1,
             latitude: res.location.lat,
@@ -65,6 +65,31 @@ Page({
             name: res.address
           }]
         })
+      },
+      fail: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  // 选择城市
+  selcity: function () {
+    var that = this;
+    qqmapsdk.geocoder({
+      
+      address: '河北省石家庄市',
+      //逆解析成功回调函数
+      success: function (res) {
+        console.log(res)
+        var res = res.result;
+        // that.setData({
+        //   address: res.address,
+        //   markers: [{
+        //     id: 1,
+        //     latitude: res.location.lat,
+        //     longitude: res.location.lng,
+        //     name: res.address
+        //   }]
+        // })
       },
       fail: function (res) {
         console.log(res)
